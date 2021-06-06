0c85ed38425305c894a1b2d6b751a2cc88c8293c
alessandro-holik <alessandro.holik@outlook.com>
2021-06-06 15:01:32 +0200

    Initial Commit.

---
+++ src/main/kotlin/Loader.kt
@@ -0,0 +1,13 @@
+import org.bukkit.event.Listener
+import org.bukkit.plugin.java.JavaPlugin
+
+public class Loader : JavaPlugin(), Listener{
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
