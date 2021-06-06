6f290429312a5a2fe932937303d24053b649dcac
alessandro-holik <alessandro.holik@outlook.com>
2021-06-06 17:43:25 +0200

    Ignore project configuration.

--- src/main/kotlin/Loader.kt
+++ src/main/kotlin/Loader.kt
@@ -1,3 +1,5 @@
+package main
+
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -5,6 +7,7 @@ public class Loader : JavaPlugin(), Listener{
 
     override fun onEnable() {
         super.onEnable()
+        println("Hello World!")
     }
 
     override fun onDisable() {
